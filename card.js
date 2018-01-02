cb2162fb0630eb75c78a4cdede99fef0a8589220
pronyecz <pronyecz@gmail.com>
2018-01-02 17:53:05 +0100

    add envelope-card files

---
+++ card.js
@@ -0,0 +1,43 @@
+
+
+
+$(document).ready(function(){
+
+			$(".btn-open").on("click", function(){
+
+				$(".contain").css({"transform":" rotateX( -270deg )" });
+				$(".btn-open").fadeOut(700);
+				$(".btn-closed").fadeIn(700);
+				$(".card").delay(1500).animate({"bottom":"215"},700, 
+
+					function(){ 
+					$(".card").css({"box-shadow": "2px -1px 10px rgb(30, 30, 30)"})
+				});
+			});
+
+			$(".btn-closed").on("click", function(){
+
+				$(".btn-open").delay(1500).fadeIn(700);
+				$(".btn-closed").fadeOut(700);
+				$(".card").animate({"bottom":"0"},700, 
+
+					function(){
+					$(".card").css({"box-shadow": "none"});
+					$(".contain").css({"transform":" rotateX( 0deg )" });
+					});
+				});
+
+			$("section").hover(function(){},
+
+					function(){ 
+				$(".btn-open").delay(1500).fadeIn(700);
+				$(".btn-closed").fadeOut(700);
+				$(".card").animate({"bottom":"0"},700, 
+
+					function(){
+					$(".card").css({"box-shadow": "none"});
+					$(".contain").css({"transform":" rotateX( 0deg )" });
+				});
+			});
+		});
+
